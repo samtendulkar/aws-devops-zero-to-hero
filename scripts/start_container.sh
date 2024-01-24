23a84751e005d852555f22783795c1ea55b56382
samtendulkar <87378440+samtendulkar@users.noreply.github.com>
2024-01-25 00:24:29 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull tendulkarsam/simple-python-flask
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 docker pull tendulkarsam/simple-python-flask
+docker run -d -p 5000:5000 tendulkarsam/simple-python-flask
